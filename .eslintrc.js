3281999e7ea46bc8759be0adc2a316982d2e8fb7
Sarup Banskota <sbanskota08@gmail.com>
2017-09-21 13:22:11 +0700

    Introduce Ember suave

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,16 +2,43 @@ module.exports = {
   root: true,
   parserOptions: {
     ecmaVersion: 2017,
-    sourceType: 'module'
+    sourceType: "module"
   },
-  extends: 'eslint:recommended',
+  extends: ["prettier"],
   env: {
     browser: true
   },
-  plugins: [
-    "prettier"
-  ],
+  plugins: ["prettier", "ember-suave"],
   rules: {
-    "prettier/prettier": "error"
+    // Formatting
+    "prettier/prettier": "error",
+
+    // ES6
+    "arrow-parens": ["error", "always"],
+    "generator-star-spacing": [
+      "error",
+      {
+        before: false,
+        after: true
+      }
+    ],
+    "no-var": "error",
+    "object-shorthand": ["error", "always"],
+    "prefer-spread": "error",
+    "prefer-template": "error",
+
+    // Overrides for Ember
+    "new-cap": [
+      "error",
+      {
+        capIsNewExceptions: ["A"]
+      }
+    ],
+
+    "ember-suave/no-const-outside-module-scope": "error",
+    "ember-suave/no-direct-property-access": "error",
+    "ember-suave/prefer-destructuring": "error",
+    "ember-suave/require-access-in-comments": "error",
+    "ember-suave/require-const-for-ember-properties": "error"
   }
 };
